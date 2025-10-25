ad0bdce1a2416f709be74d73f4a9ec73fa709836
Jagath R <jagathranganathan@gmail.com>
2025-10-25 14:14:30 +0530

    Optimize transaction import performance by batching database operations
    - Pre-parse full CSV data on file selection to avoid re-parsing during import
    - Batch insert missing categories instead of individual inserts
    - Batch insert all valid transactions instead of individual inserts
    - Reduces import time significantly by eliminating N+1 query problem

---
+++ dist/assets/papaparse.min-Cew4rEdx.js
@@ -0,0 +1,1 @@
+import{c as r}from"./vendor-CeoOk9QE.js";import{r as a}from"./utils-Bk4gEjOa.js";var p=a();const t=r(p);export{t as P};
---
+++ dist/assets/useTransactions-DNRskSPt.js
@@ -0,0 +1,1 @@
+import{c as s}from"./index-Da4dX3Ws.js";const n=()=>s();export{n as u};
